0c99611a73e9c9604ca7c04c45996ba3696d28d4
Delaney McLachlan <dmclachl@uwo.ca>
2023-09-21 20:36:12 -0400

    Added gitignore, js file with JSON data structure

---
+++ se3316lab1/script.js
@@ -0,0 +1,25 @@
+//adding JavaScript file for backend programming
+
+const countriesJSON = [                      //JSON data structure for searching, name and currencies properties
+    { name: "Singapore", currency: "SGD" },
+    { name: "Slovakia", currency: "EUR" },
+    { name: "Slovenia", currency: "EUR" },
+    { name: "Solomon Islands", currency: "SBD" },
+    { name: "Somalia", currency: "SOS" },
+    { name: "South Africa", currency: "ZAR" },
+    { name: "South Sudan", currency: "SSP" },
+    { name: "Spain", currency: "EUR" },
+    { name: "Sri Lanka", currency: "LKR" },
+    { name: "Sudan", currency: "SDG" },
+    { name: "Suriname", currency: "SRD" },
+    { name: "Sweden", currency: "SEK" },
+    { name: "Switzerland", currency: "CHF" },
+    { name: "Syrian Arab Republic", currency: "SYP" },
+    { name: "Tajikistan", currency: "TJS" },
+    { name: "Tanzania", currency: "TZS" },
+    { name: "Thailand", currency: "THB" },
+    { name: "Timor-Leste", currency: "USD" },
+    { name: "Togo", currency: "XOF" },
+    { name: "Tonga", currency: "TOP" },
+  ];
+  
